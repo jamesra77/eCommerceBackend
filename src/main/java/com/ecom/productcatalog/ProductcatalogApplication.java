20c0260f6a20894b55d1266a66ece7ccab895db8
James Amos <james.amos7@gmail.com>
2025-01-21 17:14:48 -0500

    didn't really change much, just added some logger configs in application.properties

--- src/main/java/com/ecom/productcatalog/ProductcatalogApplication.java
+++ src/main/java/com/ecom/productcatalog/ProductcatalogApplication.java
@@ -3,7 +3,7 @@ package com.ecom.productcatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ecom.productcatalog")
 public class ProductcatalogApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ecom/productcatalog/respository/CategoryRepository.java
+++ src/main/java/com/ecom/productcatalog/respository/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.ecom.productcatalog.respository;
 
 import com.ecom.productcatalog.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
