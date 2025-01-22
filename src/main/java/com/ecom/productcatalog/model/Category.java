8236d03c96d40b461c2795d3a30e7609c6822bcc
James Amos <james.amos7@gmail.com>
2025-01-21 22:04:44 -0500

    second commit

--- src/main/java/com/ecom/productcatalog/model/Category.java
+++ src/main/java/com/ecom/productcatalog/model/Category.java
@@ -1,5 +1,6 @@
 package com.ecom.productcatalog.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class Category {
     private Long id;
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category",
                 cascade = CascadeType.ALL,
                 fetch = FetchType.LAZY)
